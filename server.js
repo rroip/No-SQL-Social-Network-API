773c8c78dc80847c0e48cd167b5dda38b0977086
Stan Sitnitski <stanley@Stans-MBP.socal.rr.com>
2022-11-04 14:34:50 -0700

    initial push with models and server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// mangoose.connect
+
+
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
